014085c1603426b0a1ff9ef7d0afcf9252a37c4b
KazmirBO <sebastian.kolanowski@op.pl>
2021-11-05 00:44:56 +0100

    Youtube app v0.2.1 - "New Interface + Code Optimize"

--- Python/Youtube.py
+++ Python/Youtube.py
@@ -10,10 +10,11 @@ import pafy
 import youtube_dl
 import platform
 import time
+import webbrowser
 from pathlib import Path
 from youtube_search import YoutubeSearch
 from PyQt5 import QtCore
-from PyQt5.QtCore import (Qt, QTimer)
+from PyQt5.QtCore import (Qt, QTimer, QThread)
 from PyQt5.QtGui import (QFont, QImage, QPixmap)
 from PyQt5.QtWidgets import (QHBoxLayout, QVBoxLayout, QLineEdit, QSlider,
                              QPushButton, QLabel, QWidget, QMainWindow,
@@ -21,8 +22,43 @@ from PyQt5.QtWidgets import (QHBoxLayout, QVBoxLayout, QLineEdit, QSlider,
                              )
 
 
-__version__ = 'v0.1.14 - "Timestamp"'
+__version__ = 'v0.2.1 - "New Interface + Code Optimize"'
 __author__ = 'Sebastian Kolanowski'
+
+
+class _DownloadMP3(QThread):
+    notifyProgress = QtCore.pyqtSignal(int)
+
+    def __init__(self, id, path, parent=None):
+        QThread.__init__(self, parent)
+        self.id = id
+        self.path = path
+
+    def run(self):
+        ydl_opts = {'outtmpl': self.path + '%(title)s.%(ext)s',
+                    'audio-format': 'bestaudio',
+                    'postprocessors': [{
+                        'key': 'FFmpegExtractAudio',
+                        'preferredcodec': 'mp3',
+                        'preferredquality': '192',
+                        }], }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([self.id])
+
+
+class _DownloadMP4(QThread):
+    notifyProgress = QtCore.pyqtSignal(int)
+
+    def __init__(self, id, path, parent=None):
+        QThread.__init__(self, parent)
+        self.id = id
+        self.path = path
+
+    def run(self):
+        ydl_opts = {'outtmpl': self.path + '%(title)s.%(ext)s',
+                    'format': 'best'}
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([self.id])
 
 
 class Window(QMainWindow):
@@ -31,27 +67,28 @@ class Window(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         Title = "Youtube App"
-        self.setFont(QFont('PatrickHand', 12))
         self.setWindowTitle(Title)
-        self.setFixedWidth(650)
+        self.setFixedWidth(700)
         self.setFixedHeight(100)
         self.generalLayout = QHBoxLayout()
         _centralWidget = QWidget(self)
         _centralWidget.setLayout(self.generalLayout)
         self.setCentralWidget(_centralWidget)
         self.timer = QTimer(self)
-        self.showed = 0
-        self.duration = 0
+        self.i = 0
         self.queue = []
         self.ifSkip = 0
-        self.i = 0
+        self.showed = 0
+        self.duration = 0
         # <===> PLATFORMA/SYSTEM OPERACYJNY <===>
         self.platform = platform.system()
         self.downloads_path = str(Path.home() / "Downloads")
         if self.platform == "Windows":
             self.downloads_path += "\\"
+            self.setFont(QFont('Calibri', 12))
         else:
             self.downloads_path += "/"
+            self.setFont(QFont('PatrickHand', 12))
         # <===> PLATFORMA/SYSTEM OPERACYJNY <===>
         self.Instance = vlc.Instance('--no-video')
         self.player = self.Instance.media_player_new()
@@ -65,8 +102,8 @@ class Window(QMainWindow):
 
         self.butt1 = QPushButton("Pauza/Wznów")
         self.butt2 = QPushButton("Zatrzymaj")
-        self.volText = QLabel("Volume: ")
-        self.volText.setFixedWidth(50)
+        self.volText = QLabel("Głośność 🔈: ")
+        self.volText.setFixedWidth(80)
         self.volVal = QLabel()
         self.volVal.setText("100")
         self.volVal.setFixedWidth(30)
@@ -95,8 +132,8 @@ class Window(QMainWindow):
         self.strona = QLabel()
         self.strona.setText(str(self.i+1))
         self.strona.setAlignment(QtCore.Qt.AlignCenter)
-        self.left = QPushButton("Poprzednia")
-        self.right = QPushButton("Następna")
+        self.left = QPushButton("⬅️ Poprzednia")
+        self.right = QPushButton("Następna ➡️")
         self.right.clicked.connect(self._increase)
         self.left.clicked.connect(self._decrease)
         self.page = QHBoxLayout()
@@ -117,7 +154,7 @@ class Window(QMainWindow):
 
         self.musicProgress = QHBoxLayout()
         self.skip = QPushButton("Skip")
-        self.skip.setFixedWidth(75)
+        self.skip.setFixedWidth(80)
         self.progVal = QLabel("0/0")
         self.progVal.setFixedWidth(90)
         self.musicBar = QSlider(Qt.Horizontal)
@@ -134,91 +171,97 @@ class Window(QMainWindow):
 
         self.image1 = QImage()
         self.imgL1 = QLabel()
-        self.downMp1 = QPushButton("Pobierz mp3")
-        self.downMp1.setFixedWidth(200)
-        self.downMv1 = QPushButton("Pobierz mp4")
-        self.downMv1.setFixedWidth(200)
-        self.p1 = QPushButton("Odtwórz")
-        self.p1.setFixedWidth(200)
+        self.downMp1 = QPushButton("MP3 ⬇")
+        self.downMv1 = QPushButton("MP4 ⬇")
+        self.p1 = QPushButton("ODT ▶️")
+        self.p1.setFixedWidth(100)
+        self.yt1 = QPushButton("YT 🌍")
+        self.yt1.setFixedWidth(100)
 
         self.image2 = QImage()
         self.imgL2 = QLabel()
-        self.downMp2 = QPushButton("Pobierz mp3")
-        self.downMp2.setFixedWidth(200)
-        self.downMv2 = QPushButton("Pobierz mp4")
-        self.downMv2.setFixedWidth(200)
-        self.p2 = QPushButton("Odtwórz")
-        self.p2.setFixedWidth(200)
+        self.downMp2 = QPushButton("MP3 ⬇")
+        self.downMv2 = QPushButton("MP4 ⬇")
+        self.p2 = QPushButton("ODT ▶️")
+        self.p2.setFixedWidth(100)
+        self.yt2 = QPushButton("YT 🌍")
+        self.yt2.setFixedWidth(100)
 
         self.image3 = QImage()
         self.imgL3 = QLabel()
-        self.downMp3 = QPushButton("Pobierz mp3")
-        self.downMp3.setFixedWidth(200)
-        self.downMv3 = QPushButton("Pobierz mp4")
-        self.downMv3.setFixedWidth(200)
-        self.p3 = QPushButton("Odtwórz")
-        self.p3.setFixedWidth(200)
+        self.downMp3 = QPushButton("MP3 ⬇")
+        self.downMv3 = QPushButton("MP4 ⬇")
+        self.p3 = QPushButton("ODT ▶️")
+        self.p3.setFixedWidth(100)
+        self.yt3 = QPushButton("YT 🌍")
+        self.yt3.setFixedWidth(100)
 
         self.image4 = QImage()
         self.imgL4 = QLabel()
-        self.downMp4 = QPushButton("Pobierz mp3")
-        self.downMp4.setFixedWidth(200)
-        self.downMv4 = QPushButton("Pobierz mp4")
-        self.downMv4.setFixedWidth(200)
-        self.p4 = QPushButton("Odtwórz")
-        self.p4.setFixedWidth(200)
+        self.downMp4 = QPushButton("MP3 ⬇")
+        self.downMv4 = QPushButton("MP4 ⬇")
+        self.p4 = QPushButton("ODT ▶️")
+        self.p4.setFixedWidth(100)
+        self.yt4 = QPushButton("YT 🌍")
+        self.yt4.setFixedWidth(100)
 
         self.image5 = QImage()
         self.imgL5 = QLabel()
-        self.downMp5 = QPushButton("Pobierz mp3")
-        self.downMp5.setFixedWidth(200)
-        self.downMv5 = QPushButton("Pobierz mp4")
-        self.downMv5.setFixedWidth(200)
-        self.p5 = QPushButton("Odtwórz")
-        self.p5.setFixedWidth(200)
+        self.downMp5 = QPushButton("MP3 ⬇")
+        self.downMv5 = QPushButton("MP4 ⬇")
+        self.p5 = QPushButton("ODT ▶️")
+        self.p5.setFixedWidth(100)
+        self.yt5 = QPushButton("YT 🌍")
+        self.yt5.setFixedWidth(100)
 
         self.image6 = QImage()
         self.imgL6 = QLabel()
-        self.downMp6 = QPushButton("Pobierz mp3")
-        self.downMp6.setFixedWidth(200)
-        self.downMv6 = QPushButton("Pobierz mp4")
-        self.downMv6.setFixedWidth(200)
-        self.p6 = QPushButton("Odtwórz")
-        self.p6.setFixedWidth(200)
+        self.downMp6 = QPushButton("MP3 ⬇")
+        self.downMv6 = QPushButton("MP4 ⬇")
+        self.p6 = QPushButton("ODT ▶️")
+        self.p6.setFixedWidth(100)
+        self.yt6 = QPushButton("YT 🌍")
+        self.yt6.setFixedWidth(100)
 
 # <--------------------------------------------------------------------------->
 
-        self.downMp1.clicked.connect(lambda: self._do(self.id[(self.i*6)+0]))
+        self.downMp1.clicked.connect(lambda: self._do(
+            self.id[(self.i*6)+0], self.downMp1))
         self.downMv1.clicked.connect(lambda: self._dv(self.id[(self.i*6)+0]))
         self.p1.clicked.connect(
             lambda: self._pl(self.id[(self.i*6)+0], self.title[(self.i*6)+0],
                              self.time[(self.i*6)+0]))
 
-        self.downMp2.clicked.connect(lambda: self._do(self.id[(self.i*6)+1]))
+        self.downMp2.clicked.connect(lambda: self._do(
+            self.id[(self.i*6)+1], self.downMp2))
         self.downMv2.clicked.connect(lambda: self._dv(self.id[(self.i*6)+1]))
         self.p2.clicked.connect(
             lambda: self._pl(self.id[(self.i*6)+1], self.title[(self.i*6)+1],
                              self.time[(self.i*6)+1]))
 
-        self.downMp3.clicked.connect(lambda: self._do(self.id[(self.i*6)+2]))
+        self.downMp3.clicked.connect(lambda: self._do(
+            self.id[(self.i*6)+2], self.downMp3))
         self.downMv3.clicked.connect(lambda: self._dv(self.id[(self.i*6)+2]))
         self.p3.clicked.connect(
             lambda: self._pl(self.id[(self.i*6)+2], self.title[(self.i*6)+2],
                              self.time[(self.i*6)+2]))
 
-        self.downMp4.clicked.connect(lambda: self._do(self.id[(self.i*6)+3]))
+        self.downMp4.clicked.connect(lambda: self._do(
+            self.id[(self.i*6)+3], self.downMp4))
         self.downMv1.clicked.connect(lambda: self._dv(self.id[(self.i*6)+3]))
         self.p4.clicked.connect(
             lambda: self._pl(self.id[(self.i*6)+3], self.title[(self.i*6)+3],
                              self.time[(self.i*6)+3]))
 
-        self.downMp5.clicked.connect(lambda: self._do(self.id[(self.i*6)+4]))
+        self.downMp5.clicked.connect(lambda: self._do(
+            self.id[(self.i*6)+4], self.downMp5))
         self.downMv5.clicked.connect(lambda: self._dv(self.id[(self.i*6)+4]))
         self.p5.clicked.connect(
             lambda: self._pl(self.id[(self.i*6)+4], self.title[(self.i*6)+4],
                              self.time[(self.i*6)+4]))
 
-        self.downMp6.clicked.connect(lambda: self._do(self.id[(self.i*6)+5]))
+        self.downMp6.clicked.connect(lambda: self._do(
+            self.id[(self.i*6)+5], self.downMp6))
         self.downMv6.clicked.connect(lambda: self._dv(self.id[(self.i*6)+5]))
         self.p6.clicked.connect(
             lambda: self._pl(self.id[(self.i*6)+5], self.title[(self.i*6)+5],
@@ -228,32 +271,68 @@ class Window(QMainWindow):
 
         self.video = QGridLayout()
 
+        self.do1 = QHBoxLayout()
+        self.do1.addWidget(self.downMp1)
+        self.do1.addWidget(self.downMv1)
+        self.do2 = QHBoxLayout()
+        self.do2.addWidget(self.downMp2)
+        self.do2.addWidget(self.downMv2)
+        self.do3 = QHBoxLayout()
+        self.do3.addWidget(self.downMp3)
+        self.do3.addWidget(self.downMv3)
+        self.do4 = QHBoxLayout()
+        self.do4.addWidget(self.downMp4)
+        self.do4.addWidget(self.downMv4)
+        self.do5 = QHBoxLayout()
+        self.do5.addWidget(self.downMp5)
+        self.do5.addWidget(self.downMv5)
+        self.do6 = QHBoxLayout()
+        self.do6.addWidget(self.downMp6)
+        self.do6.addWidget(self.downMv6)
+
+# <--------------------------------------------------------------------------->
+
+        self.od1 = QHBoxLayout()
+        self.od1.addWidget(self.p1)
+        self.od1.addWidget(self.yt1)
+        self.od2 = QHBoxLayout()
+        self.od2.addWidget(self.p2)
+        self.od2.addWidget(self.yt2)
+        self.od3 = QHBoxLayout()
+        self.od3.addWidget(self.p3)
+        self.od3.addWidget(self.yt3)
+        self.od4 = QHBoxLayout()
+        self.od4.addWidget(self.p4)
+        self.od4.addWidget(self.yt4)
+        self.od5 = QHBoxLayout()
+        self.od5.addWidget(self.p5)
+        self.od5.addWidget(self.yt5)
+        self.od6 = QHBoxLayout()
+        self.od6.addWidget(self.p6)
+        self.od6.addWidget(self.yt6)
+
+# <--------------------------------------------------------------------------->
+
         self.video.addWidget(self.imgL1, 0, 0)
-        self.video.addWidget(self.downMp1, 1, 0)
-        self.video.addWidget(self.downMv1, 2, 0)
-        self.video.addWidget(self.p1, 3, 0)
-        self.video.addWidget(self.imgL4, 4, 0)
-        self.video.addWidget(self.downMp4, 5, 0)
-        self.video.addWidget(self.downMv4, 6, 0)
-        self.video.addWidget(self.p4, 7, 0)
+        self.video.addLayout(self.do1, 1, 0)
+        self.video.addLayout(self.od1, 2, 0)
+        self.video.addWidget(self.imgL4, 3, 0)
+        self.video.addLayout(self.do4, 4, 0)
+        self.video.addLayout(self.od4, 5, 0)
 
         self.video.addWidget(self.imgL2, 0, 1)
-        self.video.addWidget(self.downMp2, 1, 1)
-        self.video.addWidget(self.downMv2, 2, 1)
-        self.video.addWidget(self.p2, 3, 1)
-        self.video.addWidget(self.imgL5, 4, 1)
-        self.video.addWidget(self.downMp5, 5, 1)
-        self.video.addWidget(self.downMv5, 6, 1)
-        self.video.addWidget(self.p5, 7, 1)
+        self.video.addLayout(self.do2, 1, 1)
+        self.video.addLayout(self.od2, 2, 1)
+        self.video.addWidget(self.imgL5, 3, 1)
+        self.video.addLayout(self.do5, 4, 1)
+        self.video.addLayout(self.od5, 5, 1)
 
         self.video.addWidget(self.imgL3, 0, 2)
-        self.video.addWidget(self.downMp3, 1, 2)
-        self.video.addWidget(self.downMv3, 2, 2)
-        self.video.addWidget(self.p3, 3, 2)
-        self.video.addWidget(self.imgL6, 4, 2)
-        self.video.addWidget(self.downMp6, 5, 2)
-        self.video.addWidget(self.downMv6, 6, 2)
-        self.video.addWidget(self.p6, 7, 2)
+        self.video.addLayout(self.do3, 1, 2)
+        self.video.addLayout(self.od3, 2, 2)
+        self.video.addWidget(self.imgL6, 3, 2)
+        self.video.addLayout(self.do6, 4, 2)
+        self.video.addLayout(self.od6, 5, 2)
 
 # <--------------------------------------------------------------------------->
 
@@ -263,8 +342,9 @@ class Window(QMainWindow):
 
         self.id = []
         self.time = []
-        self.mylist = []
         self.title = []
+        self.mylist = []
+        self.channel = []
         self.results = YoutubeSearch("'" + self.text.text() + "'",
                                      max_results=60).to_dict()
 
@@ -273,6 +353,7 @@ class Window(QMainWindow):
             self.time.append(v['duration'])
             self.title.append(v['title'])
             self.mylist.append(v['thumbnails'][0])
+            self.channel.append(v['channel'])
 
         self.i = 0
         self.strona.setText(str(self.i+1))
@@ -297,7 +378,7 @@ class Window(QMainWindow):
 
     def _updateUi(self):
         if self.showed == 0:
-            self.setFixedHeight(800)
+            self.setFixedHeight(780)
             self.main.addLayout(self.video)
             self.main.addLayout(self.page)
             self.main.addWidget(self.played)
@@ -313,6 +394,19 @@ class Window(QMainWindow):
 
             self.timer.timeout.connect(self._music)
             self.timer.start(100)
+
+            self.yt1.clicked.connect(lambda: webbrowser.open(
+                "https://www.youtube.com/watch?v=" + self.id[(self.i*6)+0]))
+            self.yt2.clicked.connect(lambda: webbrowser.open(
+                "https://www.youtube.com/watch?v=" + self.id[(self.i*6)+1]))
+            self.yt3.clicked.connect(lambda: webbrowser.open(
+                "https://www.youtube.com/watch?v=" + self.id[(self.i*6)+2]))
+            self.yt4.clicked.connect(lambda: webbrowser.open(
+                "https://www.youtube.com/watch?v=" + self.id[(self.i*6)+3]))
+            self.yt5.clicked.connect(lambda: webbrowser.open(
+                "https://www.youtube.com/watch?v=" + self.id[(self.i*6)+4]))
+            self.yt6.clicked.connect(lambda: webbrowser.open(
+                "https://www.youtube.com/watch?v=" + self.id[(self.i*6)+5]))
 
         self.showed = 1
 
@@ -360,32 +454,38 @@ class Window(QMainWindow):
 
 # <--------------------------------------------------------------------------->
 
-# <===> TESTED <=================================================> TESTED <===>
-
-    def _do(self, id):
-        ydl_opts = {'outtmpl': self.downloads_path + '%(title)s.%(ext)s',
-                    'audio-format': 'bestaudio',
-                    'postprocessors': [{
-                        'key': 'FFmpegExtractAudio',
-                        'preferredcodec': 'mp3',
-                        'preferredquality': '192',
-                        }], }
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([id])
-
-# <===> TESTED <=================================================> TESTED <===>
+        self.imgL1.setToolTip("Tytuł: " + self.title[(self.i*6)+0]
+                              + "\nKanał: " + self.channel[(self.i*6)+0])
+        self.imgL2.setToolTip("Tytuł: " + self.title[(self.i*6)+1]
+                              + "\nKanał: " + self.channel[(self.i*6)+1])
+        self.imgL3.setToolTip("Tytuł: " + self.title[(self.i*6)+2]
+                              + "\nKanał: " + self.channel[(self.i*6)+2])
+        self.imgL4.setToolTip("Tytuł: " + self.title[(self.i*6)+3]
+                              + "\nKanał: " + self.channel[(self.i*6)+3])
+        self.imgL5.setToolTip("Tytuł: " + self.title[(self.i*6)+4]
+                              + "\nKanał: " + self.channel[(self.i*6)+4])
+        self.imgL6.setToolTip("Tytuł: " + self.title[(self.i*6)+5]
+                              + "\nKanał: " + self.channel[(self.i*6)+5])
 
 # <--------------------------------------------------------------------------->
 
-# <===> TESTED <=================================================> TESTED <===>
+# <===> WORK IN PROGRESS <=============================> WORK IN PROGRESS <===>
+
+    def _do(self, id, button):
+        self.worker = _DownloadMP3(id, self.downloads_path)
+        self.worker.start()
+
+# <===> WORK IN PROGRESS <=============================> WORK IN PROGRESS <===>
+
+# <--------------------------------------------------------------------------->
+
+# <===> WORK IN PROGRESS <=============================> WORK IN PROGRESS <===>
 
     def _dv(self, id):
-        ydl_opts = {'outtmpl': self.downloads_path + '%(title)s.%(ext)s',
-                    'format': 'best'}
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([id])
+        self.worker = _DownloadMP4(id, self.downloads_path)
+        self.worker.start()
 
-# <===> TESTED <=================================================> TESTED <===>
+# <===> WORK IN PROGRESS <=============================> WORK IN PROGRESS <===>
 
 # <--------------------------------------------------------------------------->
 
