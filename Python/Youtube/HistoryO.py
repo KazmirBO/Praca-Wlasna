6a109f4c56b8d8b236cba83562c9ca4a2f4fdc4f
KazmirBO <sebastian.kolanowski@op.pl>
2021-12-20 19:54:19 +0100

    History functions

--- Python/Youtube/HistoryO.py
+++ Python/Youtube/HistoryO.py
@@ -1,62 +1,51 @@
 import os
-from PyQt5.QtWidgets import (QVBoxLayout, QWidget, QLabel, QHBoxLayout,
-                             QComboBox, QPushButton)
+from PyQt5.QtWidgets import (
+    QVBoxLayout,
+    QTableWidget,
+    QWidget,
+    QLabel,
+    QHBoxLayout,
+    QPushButton,
+    QTableWidgetItem
+)
 
 # Plik z klasą i funkcjami do obsługi historii.
 
 
 class oknoHistorii(QWidget):
-    def __init__(self, history, tempKat):
+    def __init__(self, tempOdt):
         super().__init__()
         self.resize(400, 100)
-        self.history = history
-        self.tempKat = tempKat
+        self.tempOdt = tempOdt
         self.layout = QVBoxLayout()
-        self.tekst = QLabel("Wybierz, który rekord usunąć:")
-        self.rekord = QComboBox()
+        self.table = QTableWidget()
+        self.count = 0
+        self.dane = []
 
-        self.polePrzy = QHBoxLayout()
-        self.usun = QPushButton("Usuń")
-        self.usun.clicked.connect(self._usun)
+        if os.path.isfile(self.tempOdt):
+            with open(self.tempOdt) as f:
+                for line in f:
+                    self.dane.append(line.rstrip('\n'))
+
+        self.table.setColumnCount(3)
+        self.table.setRowCount(int(len(self.dane)/3))
+        self.table.setMinimumWidth(500)
+        self.table.setMinimumHeight(500)
+        self.table.setHorizontalHeaderLabels(["Id", "Tytuł", "Czas Trwania"])
+
+        print(int(len(self.dane)))
+        for i in range(0, int(len(self.dane)/3)):
+            for j in range(0, 3):
+                self.table.setItem(
+                    i, j, QTableWidgetItem(str(self.dane[self.count])))
+                self.count += 1
+
+        self.table.resizeColumnsToContents()
+        self.table.resizeRowsToContents()
+
         self.exit = QPushButton("Wyjdź")
         self.exit.clicked.connect(lambda: self.close())
-        self.polePrzy.addWidget(self.usun)
-        self.polePrzy.addWidget(self.exit)
 
-        for i in range(0, self.history.count()):
-            self.rekord.addItem(self.history.itemText(i))
-
-        self.layout.addWidget(self.tekst)
-        self.layout.addWidget(self.rekord)
-        self.layout.addLayout(self.polePrzy)
+        self.layout.addWidget(self.table)
+        self.layout.addWidget(self.exit)
         self.setLayout(self.layout)
-
-    def _usun(self):
-        if os.path.isfile(self.tempKat):
-            f = open(self.tempKat, 'r')
-            lst = []
-            for line in f:
-                if str(self.rekord.currentText()) + "\n" in line:
-                    line = line.replace(self.rekord.currentText(), '')
-                lst.append(line)
-            f.close()
-            f = open(self.tempKat, 'w')
-            for line in lst:
-                f.write(line)
-            f.close()
-            self.rekord.removeItem(self.rekord.findData(
-                self.rekord.currentText())
-            )
-
-            self.history.clear()
-            self.rekord.clear()
-
-            if os.path.isfile(self.tempKat):
-                historia = []
-                with open(self.tempKat, 'r') as f:
-                    historia = f.read().splitlines()
-                f.close()
-                for f in historia:
-                    if not f == '':
-                        self.history.addItem(f)
-                        self.rekord.addItem(f)
