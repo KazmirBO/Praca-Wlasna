35f8edc1a3fc8c54fe1bdf1693b5d329ab2d73cb
Kazmir_BO <sebastian.kolanowski@op.pl>
2022-06-21 22:52:19 +0200

    C++

---
+++ C++/Zadania/Jaca/Nazwisko_Imie.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+class Nazwisko {
+private:
+  char *nazwisko;
+
+public:
+  Nazwisko() { nazwisko = ""; }
+  Nazwisko(char *nz) {
+    nazwisko = nz;
+    std::cout << "Nazwisko: " << nazwisko << std::endl;
+  }
+  ~Nazwisko() { std::cout << "Nazwisko: " << nazwisko << std::endl; }
+};
+class Pelne : public Nazwisko {
+private:
+  char *imie;
+
+public:
+  Pelne() { imie = ""; }
+  Pelne(char *im, char *nz) : Nazwisko(nz) {
+    imie = im;
+    std::cout << "Imie: " << imie << std::endl;
+  }
+  ~Pelne() { std::cout << "Imie: " << imie << std::endl; }
+};
+int main(int argc, char const *argv[]) {
+  char *im = "A", *nz = "A";
+  Pelne osoba = Pelne(im, nz);
+  return 0;
+}
